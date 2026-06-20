f2211e7f6f4ab5c5abcfb61b23f271c4737ea4c6
RohitMaurya-16 <quickhelp36@gmail.com>
2026-06-21 02:49:09 +0530

    add 04 cards project fix

---
+++ 04-cards-project/src/components/Card.jsx
@@ -0,0 +1,30 @@
+
+import {Bookmark} from 'lucide-react';
+ const Card = (prop) => {
+  return (
+    <div className='card'>
+          <div>
+            <div className="top">
+          <img src={prop.logo}/>
+          <button>Save <Bookmark  size={12}/></button>
+          </div>
+          <div className="center">
+<h3>{prop.name}<span>{prop.days}</span></h3>
+<h2>{prop.title}</h2>
+            <div className='tag'>
+              <h4>{prop.level}</h4> 
+            </div>
+          </div>
+          </div>
+          <div className="bottom">
+              <div>
+                <h3>{prop.salary}</h3>
+                <p>{prop.location}</p>
+              </div>
+              <button>Apply Now</button>
+           </div>
+          </div>
+  )
+};
+
+export default Card;
